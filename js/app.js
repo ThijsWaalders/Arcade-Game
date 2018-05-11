a69c9a14e7275bac1699fc3bce5ea3d16bb66924
ThijsWaalders <henk418@gmail.com>
2018-05-11 15:29:06 +0200

    feat: instantiated the objects

--- js/app.js
+++ js/app.js
@@ -32,12 +32,60 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-cat-girl.png';
+};
 
+Player.prototype.update = function() {
+    // Prevent player from moving out of canvas
+    if (this.y > 380) {
+        this.y = 380;
+    } if (this.x > 400) {
+        this.x = 400;
+    } if (this.x < 0) {
+        this.x = 0;
+    }
+    // Check for player reaching top of canvas and winning the game
+    if (this.y < 0) {
+        this.x = 200;
+        this.y = 380;
+    }
+};
+
+Player.prototype.handleInput = function(keyPress) {
+    switch (keyPress) {
+        case 'up':
+            this.y -= this.speed + 30;
+            break;
+        case 'right':
+            this.x += this.speed + 50;
+            break;
+        case 'down':
+            this.y += this.speed + 30;
+            break;
+        case 'left':
+            this.x -= this.speed + 50;
+            break;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [];
 
+// position "y" where the enemies will are created
+let enemyPosition = [60, 140, 220];
+let player = new Player(200, 380, 50);
+let enemy;
+
+enemyPosition.forEach(function(posY) {
+    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
+    allEnemies.push(enemy);
+});
 
 
 // This listens for key presses and sends the keys to your
